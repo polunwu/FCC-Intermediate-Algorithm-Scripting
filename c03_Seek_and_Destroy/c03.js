54ba07973e8d7c98c9bc0dfa2f765b191ceb1161
Polun <polunwu@gmail.com>
2020-01-02 02:01:44 +0800

    modify c03 after checking HINTS

--- c03_Seek_and_Destroy/c03.js
+++ c03_Seek_and_Destroy/c03.js
@@ -1,8 +1,15 @@
-function destroyer(arr) {
-  // get an array of remove target from 'arguments'
-  const removeList = Object.values(arguments).slice(1);
+/* ES6 */
+function destroyer(arr, ...removeList) {
   let newArr = arr.filter(item => !removeList.includes(item));
   return newArr;
 }
+
+/* Another way */
+/* function destroyer(arr) {
+  // conver 'arguments' into removeList array start from index 1
+  const removeList = Array.from(arguments).slice(1);
+  let newArr = arr.filter(item => !removeList.includes(item));
+  return newArr;
+} */
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
