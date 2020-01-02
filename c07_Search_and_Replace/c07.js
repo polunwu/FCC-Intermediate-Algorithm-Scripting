9ed8334c67bb8bcdaa9bf7bb88c13543f9edb87d
Polun <polunwu@gmail.com>
2020-01-02 17:21:09 +0800

    update c07

--- c07_Search_and_Replace/c07.js
+++ c07_Search_and_Replace/c07.js
@@ -1,10 +1,9 @@
 function myReplace(str, before, after) {
   // Check first chart at before is Uppercase
   if (/^[A-Z]/.test(before)) {
-    after = after[0].toUpperCase().concat(after.replace(/^./, ""));
+    after = after[0].toUpperCase() + after.slice(1);
   }
-  let boforeRegex = new RegExp(before, 'i');
-  let result = str.replace(boforeRegex, after);
+  let result = str.replace(before, after);
   return result;
 }
 
