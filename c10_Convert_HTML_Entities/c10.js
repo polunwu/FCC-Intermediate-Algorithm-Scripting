bd10d90f7c830b1ff6f1cdb163ebd5195cdb44ae
Polun <polunwu@gmail.com>
2020-01-02 21:17:46 +0800

    modify c10

--- c10_Convert_HTML_Entities/c10.js
+++ c10_Convert_HTML_Entities/c10.js
@@ -1,16 +1,26 @@
 function convertHTML(str) {
   // &colon;&rpar;
-  const htmlEntities = [
-    { char: "&", entity: "&amp;" },
-    { char: "<", entity: "&lt;" },
-    { char: ">", entity: "&gt;" },
-    { char: "\"", entity: "&quot;" },
-    { char: "'", entity: "&apos;" }
-  ];
-  htmlEntities.forEach(valPair => {
-    str = str.replace(new RegExp(valPair.char, "g"), valPair.entity);
-  });
-  return str;
+  let temp = str.split("");
+  for (let i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "&":
+        temp[i] = "&amp;"
+        break;
+      case "<":
+        temp[i] = "&lt;"
+        break;
+      case ">":
+        temp[i] = "&gt;"
+        break;
+      case "\"":
+        temp[i] = "&quot;"
+        break;
+      case "'":
+        temp[i] = "&apos;"
+        break;
+    }
+  }
+  return temp.join("");
 }
 
 convertHTML("Dolce & Gabbana");
